6830da6712942b4ceb23d8bc0662266c2ad5318e
MatheusRibak <matheusribak@hotmail.com>
2016-10-06 12:02:42 -0300

    Testando identação do atom

--- application/controllers/Historico.php
+++ application/controllers/Historico.php
@@ -13,30 +13,30 @@ class Historico extends MY_ControllerLogado {
 		$this->load->view('home_entidade', $data);
 	}
 
-  public function cadastrarHistorico($id_voluntario, $id_vaga){
+	public function cadastrarHistorico($id_voluntario, $id_vaga){
 
-    $this->db->select("*")
-    ->from("voluntario")
-    ->where("id_voluntario", $id_voluntario);
+		$this->db->select("*")
+		->from("voluntario")
+		->where("id_voluntario", $id_voluntario);
 
-    $teste = $query = $this->db->get()->row();
-    $id_entidade = $this->session->userdata('id_entidade');
+		$teste = $query = $this->db->get()->row();
+		$id_entidade = $this->session->userdata('id_entidade');
 
-    $data = array("dadosEntidade" => $this->Entidade_model->getEntidade($id_entidade)->row(),
-	"dadosVoluntario" => $this->Historico_model->getVoluntario($id_voluntario)->row(),
-	"dadosVaga" => $this->Vaga_model->getVagaSozinha($id_vaga));
+		$data = array("dadosEntidade" => $this->Entidade_model->getEntidade($id_entidade)->row(),
+		"dadosVoluntario" => $this->Historico_model->getVoluntario($id_voluntario)->row(),
+		"dadosVaga" => $this->Vaga_model->getVagaSozinha($id_vaga));
 
-    $this->load->view('historico', $data);
-  }
+		$this->load->view('historico', $data);
+	}
 
-  public function salvarHistorico(){
+	public function salvarHistorico(){
 		$data = array();
 		$this->Historico_model->id_voluntario =  $this->input->post('id_voluntario');
 		$this->Historico_model->historico = $this->input->post('historico');
 		$this->Historico_model->id_entidade = $this->session->userdata('id_entidade');
 
 		$this->Historico_model->Salvar($data);
-  }
+	}
 
 
 
--- application/models/vaga_model.php
+++ application/models/vaga_model.php
@@ -2,49 +2,49 @@
 
 class Vaga_model extends CI_Model {
 
-    public  $id_vaga;
-    public  $nome;
-  	public  $area;
-  	public  $descricao;
-  	public  $importancia;
-  	public  $presencial;
-  	public  $estado;
-  	public  $cidade;
-    public  $id_entidade;
-    public  $data_validade;
-    public  $ativo;
+  public  $id_vaga;
+  public  $nome;
+  public  $area;
+  public  $descricao;
+  public  $importancia;
+  public  $presencial;
+  public  $estado;
+  public  $cidade;
+  public  $id_entidade;
+  public  $data_validade;
+  public  $ativo;
 
-    function __construct() {
-        parent::__construct();
-    }
+  function __construct() {
+    parent::__construct();
+  }
 
-    public function Salvar(){
-       return $this->db->insert('vaga', $this);
-   }
+  public function Salvar(){
+    return $this->db->insert('vaga', $this);
+  }
 
-   function getVagas($teste) {
+  function getVagas($teste) {
 
-   $ativo = 'SIM';
-	 $this->db
-   ->select('*')
-   ->from('vaga')
-   ->like('nome', $teste)
-   ->where('ativo', $ativo);
+    $ativo = 'SIM';
+    $this->db
+    ->select('*')
+    ->from('vaga')
+    ->like('nome', $teste)
+    ->where('ativo', $ativo);
 
-	 return $qr = $this->db->get()->result();
+    return $qr = $this->db->get()->result();
 
-}
+  }
 
-    function getVagaSozinha($id_vaga) {
+  function getVagaSozinha($id_vaga) {
 
-    	$this->db
-      ->select("*")
-      ->from("vaga")
-      ->like('id_vaga', $id_vaga);
+    $this->db
+    ->select("*")
+    ->from("vaga")
+    ->like('id_vaga', $id_vaga);
 
-    	return $query = $this->db->get()->result();
+    return $query = $this->db->get()->result();
 
-    }
+  }
 
   public  function getVagaEntidadeAtiva() {
     $id_entidade = $this->session->userdata('id_entidade');
@@ -59,137 +59,137 @@ class Vaga_model extends CI_Model {
   }
 
   public  function getVagaEntidadeFinalizada() {
-     $id_entidade = $this->session->userdata('id_entidade');
-     $ativo = 'NAO';
-     $this->db
-     ->select("*")
-     ->from("vaga")
-     ->where('id_entidade', $id_entidade)
-     ->where('ativo', $ativo);
+    $id_entidade = $this->session->userdata('id_entidade');
+    $ativo = 'NAO';
+    $this->db
+    ->select("*")
+    ->from("vaga")
+    ->where('id_entidade', $id_entidade)
+    ->where('ativo', $ativo);
 
-     return $qr = $this->db->get()->result();
-    }
+    return $qr = $this->db->get()->result();
+  }
 
   public function recadastrarVaga($data, $id_vaga){
-      $id_entidade = $this->session->userdata('id_entidade');
-      $this->db->where('id_vaga', $id_vaga);
-      $this->db->where('id_entidade', $id_entidade);
-      $this->db->set($data);
-      return $this->db->update('vaga');
-    }
+    $id_entidade = $this->session->userdata('id_entidade');
+    $this->db->where('id_vaga', $id_vaga);
+    $this->db->where('id_entidade', $id_entidade);
+    $this->db->set($data);
+    return $this->db->update('vaga');
+  }
 
-    public function finalizarVaga($data, $id_vaga){
-        $id_entidade = $this->session->userdata('id_entidade');
-        $this->db->where('id_vaga', $id_vaga);
-        $this->db->where('id_entidade', $id_entidade);
-        $this->db->set($data);
-        return $this->db->update('vaga');
-      }
+  public function finalizarVaga($data, $id_vaga){
+    $id_entidade = $this->session->userdata('id_entidade');
+    $this->db->where('id_vaga', $id_vaga);
+    $this->db->where('id_entidade', $id_entidade);
+    $this->db->set($data);
+    return $this->db->update('vaga');
+  }
 
-    public function vagaDaEntidade(){
-      $status = 'Aguardando Reposta';
-      $id_entidade = $this->session->userdata('id_entidade');
-      $this->db
-      ->select("*")
-      ->from("vaga")
-      ->join('vaga_candidato', 'vaga.id_vaga = vaga_candidato.id_vaga')
-      ->where('status_vaga', $status);
-
-
-      return $query = $this->db->get()->result();
-    }
-
-    public function getCandidato(){
-
-      $status = 'Aguardando Reposta';
-      $this->db
-      ->select("*")
-      ->from("vaga")
-      ->join('vaga_candidato', 'vaga.id_vaga = vaga_candidato.id_vaga')
-      ->where('status_vaga', $status);
+  public function vagaDaEntidade(){
+    $status = 'Aguardando Reposta';
+    $id_entidade = $this->session->userdata('id_entidade');
+    $this->db
+    ->select("*")
+    ->from("vaga")
+    ->join('vaga_candidato', 'vaga.id_vaga = vaga_candidato.id_vaga')
+    ->where('status_vaga', $status);
 
 
-      $query = $this->db->get()->result();
+    return $query = $this->db->get()->result();
+  }
 
-      foreach ($query as $row) {
-         $this->db->select("*")
-         ->from("vaga_candidato")
-         ->where("id_vaga", $row->id_vaga);
+  public function getCandidato(){
 
-        $resultado =  $this->db->get()->result();
+    $status = 'Aguardando Reposta';
+    $this->db
+    ->select("*")
+    ->from("vaga")
+    ->join('vaga_candidato', 'vaga.id_vaga = vaga_candidato.id_vaga')
+    ->where('status_vaga', $status);
 
-        foreach ($resultado as $key) {
-          $this->db->select("*")
-          ->from("voluntario")
-          ->where("id_voluntario", $row->id_voluntario);
 
-          return $this->db->get()->result();
-        }
-      }
-    }
+    $query = $this->db->get()->result();
 
-    public function getCandidatoSozinho($id_voluntario){
-      $this->db->select("*")
-      ->from("voluntario")
-      ->where("id_voluntario", $id_voluntario);
-
-      return $this->db->get()->result();
-    }
-
-    public function vagaSozinha($id_vaga, $id_voluntario){
+    foreach ($query as $row) {
       $this->db->select("*")
       ->from("vaga_candidato")
-      ->where("id_vaga", $id_vaga)
-      ->where('id_voluntario', $id_voluntario);
+      ->where("id_vaga", $row->id_vaga);
+
+      $resultado =  $this->db->get()->result();
+
+      foreach ($resultado as $key) {
+        $this->db->select("*")
+        ->from("voluntario")
+        ->where("id_voluntario", $row->id_voluntario);
+
         return $this->db->get()->result();
+      }
     }
+  }
 
-    public function aceitarOuRecusar($id_vaga, $id_voluntario, $dados){
-      $id_entidade = $this->session->userdata('id_entidade');
-      $this->db->where('id_voluntario', $id_voluntario);
-      $this->db->where('id_vaga', $id_vaga);
-      $this->db->where('id_entidade', $id_entidade);
-      $this->db->set($dados);
-      return $this->db->update('vaga_candidato');
-    }
+  public function getCandidatoSozinho($id_voluntario){
+    $this->db->select("*")
+    ->from("voluntario")
+    ->where("id_voluntario", $id_voluntario);
 
-    public function getPorAreaUm(){
+    return $this->db->get()->result();
+  }
 
-      $id_voluntario = $this->session->userdata('id_voluntario');
+  public function vagaSozinha($id_vaga, $id_voluntario){
+    $this->db->select("*")
+    ->from("vaga_candidato")
+    ->where("id_vaga", $id_vaga)
+    ->where('id_voluntario', $id_voluntario);
+    return $this->db->get()->result();
+  }
 
-      $this->db->select("*")
-      ->from("voluntario")
-      ->where("id_voluntario", $id_voluntario);
+  public function aceitarOuRecusar($id_vaga, $id_voluntario, $dados){
+    $id_entidade = $this->session->userdata('id_entidade');
+    $this->db->where('id_voluntario', $id_voluntario);
+    $this->db->where('id_vaga', $id_vaga);
+    $this->db->where('id_entidade', $id_entidade);
+    $this->db->set($dados);
+    return $this->db->update('vaga_candidato');
+  }
 
-      $resultado_area_um = $this->db->get()->row();
+  public function getPorAreaUm(){
 
-      $this->db->select("*")
-      ->from("vaga")
-      ->where("area", $resultado_area_um->atuacao_um);
+    $id_voluntario = $this->session->userdata('id_voluntario');
 
-      return $this->db->get()->result();
+    $this->db->select("*")
+    ->from("voluntario")
+    ->where("id_voluntario", $id_voluntario);
 
+    $resultado_area_um = $this->db->get()->row();
 
-    }
+    $this->db->select("*")
+    ->from("vaga")
+    ->where("area", $resultado_area_um->atuacao_um);
 
-    public function getPorAreaDois(){
-
-      $id_voluntario = $this->session->userdata('id_voluntario');
-
-      $this->db->select("*")
-      ->from("voluntario")
-      ->where("id_voluntario", $id_voluntario);
-
-      $resultado_area_um = $this->db->get()->row();
-
-      $this->db->select("*")
-      ->from("vaga")
-      ->where("area", $resultado_area_um->atuacao_dois);
-
-      return $this->db->get()->result();
+    return $this->db->get()->result();
 
 
-    }
+  }
+
+  public function getPorAreaDois(){
+
+    $id_voluntario = $this->session->userdata('id_voluntario');
+
+    $this->db->select("*")
+    ->from("voluntario")
+    ->where("id_voluntario", $id_voluntario);
+
+    $resultado_area_um = $this->db->get()->row();
+
+    $this->db->select("*")
+    ->from("vaga")
+    ->where("area", $resultado_area_um->atuacao_dois);
+
+    return $this->db->get()->result();
+
+
+  }
 
 
 
